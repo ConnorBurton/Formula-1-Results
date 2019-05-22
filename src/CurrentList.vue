21db5bad106d01f25ccdd96900819a42f4094d59
Connor Burton <connorburton44@gmail.com>
2019-05-22 21:22:26 +0100

    Add loading animation css & js

--- src/CurrentList.vue
+++ src/CurrentList.vue
@@ -1,35 +1,40 @@
 <template>
   <div id="current-table">
-    <table>
-      <thead>
-        <tr>
-          <th>Position</th>
-          <th>Number</th>
-          <th>Driver</th>
-          <th>Constructor</th>
-          <th>Laps</th>
-          <th>Time</th>
-          <th>Points</th>
-        </tr>
-      </thead>
-      <tbody v-if="currentRace !== ''">
-        <tr v-for="detail in currentRace" >
-          <td class="position">{{detail.position}}</td>
-          <td class="number">{{detail.number}}</td>
-          <td class="name">{{detail.Driver.familyName}}</td>
-          <td class="constructor">{{detail.Constructor.name}}</td>
-          <td class="laps">{{detail.laps}}</td>
-          <td v-if="detail.Time !== undefined" class="time">{{detail.Time.time}}</td>
-          <td v-else class="time">{{detail.status}}</td>
-          <td class="points">{{detail.points}}</td>
-        </tr>
-      </tbody>
-      <div v-else>
-        No Results
-      </div>
-    </table>
-  </div>
 
+    <div class="spinner" :class="{ 'visible' : spinnerStyle}"></div>
+
+    <div :class="['results-wrapper', { 'hidden' : spinnerStyle}]">
+      <table v-if="currentRace !== null && currentRace.length !== 0">
+        <thead>
+          <tr>
+            <th>Position</th>
+            <th>Number</th>
+            <th>Driver</th>
+            <th>Constructor</th>
+            <th>Laps</th>
+            <th>Time</th>
+            <th>Points</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="detail in currentRace" >
+            <td class="position">{{detail.position}}</td>
+            <td class="number">{{detail.number}}</td>
+            <td class="name">{{detail.Driver.familyName}}</td>
+            <td class="constructor">{{detail.Constructor.name}}</td>
+            <td class="laps">{{detail.laps}}</td>
+            <td v-if="detail.Time !== undefined" class="time">{{detail.Time.time}}</td>
+            <td v-else class="time">{{detail.status}}</td>
+            <td class="points">{{detail.points}}</td>
+          </tr>
+        </tbody>
+      </table>
+      <div v-else class="no-results">
+        <p>No results were found, please select a different race</p>
+      </div>
+    </div>
+
+  </div>
 </template>
 
 <script>
@@ -37,11 +42,16 @@
     name: 'current-table',
     props: {
       currentRace: Array,
+      spinnerStyle: Boolean,
     }
   }
 </script>
 
 <style scoped>
+  #current-table {
+    position: relative;
+  }
+
   table {
     width: 100%;
     border-spacing: 0;
@@ -66,5 +76,46 @@
   }
   .position, .number {
     width: 115px;
+  }
+  .no-results {
+    display: block;
+    text-align: center;
+    padding-top: 40px;
+  }
+  .no-results p {
+    color: #fff;
+    font-size: 24px;
+    font-weight: 700;
+  }
+  .results-wrapper {
+    transform: none;
+    opacity: 1;
+    visibility: visible;
+    transition: .75s ease;
+  }
+  .results-wrapper.hidden {
+    transform: translateY(60px);
+    opacity: 0;
+    visibility: hidden;
+    transition: .3s ease;
+  }
+  .spinner {
+    position: absolute;
+    top: 40px;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 100px;
+    height: 100px;
+    background-image: url(https://thumbs.gfycat.com/BogusEmptyBrontosaurus-max-1mb.gif);
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: contain;
+    opacity: 0;
+    visibility: hidden;
+    transition: .3s ease;
+  }
+  .spinner.visible {
+    opacity: 1;
+    visibility: visible;
   }
 </style>
